28b97aa0449735be7f46049e133ca51f76685b9a
ywxk-123 <63449002+ywxk-123@users.noreply.github.com>
2023-02-12 00:02:06 +0800

    Update rpsls.py

--- rpsls.py
+++ rpsls.py
@@ -1,27 +1,42 @@
 #conding:utf-8
-"""
-第一个小项目：Rock-paper-scissors-lizard-Spock
+"""
+
+第一个小项目：Rock-paper-scissors-lizard-Spock
+
 剪刀能剪纸和蜥蜴；纸可以包住石头，驳斥Spock；石头击烂蜥蜴和剪刀；
 蜥蜴可以毒害史波克，吃纸；史波克可以砸碎剪刀，使石头蒸发。
-作者：王浩
-日期：2020.04.03
-"""
-
+作者：ywxk
 
-
-# 0 - 石头
-# 1 - 史波克
-# 2 - 纸
-# 3 - 蜥蜴
+日期：2020.04.03
+
+"""
+
+
+
+
+
+
+# 0 - 石头
+
+# 1 - 史波克
+
+# 2 - 纸
+
+# 3 - 蜥蜴
+
 # 4 - 剪刀
-
-# 以下为完成游戏所需要用到的自定义函数
+
+
+# 以下为完成游戏所需要用到的自定义函数
+
 
 import random
-
+
+
 def name_to_number(name):
     
-    '''将游戏对象对应到不同的整数'''
+    '''将游戏对象对应到不同的整数
+'''
         
     if name == '石头' :
         player_choice_number=0
@@ -33,10 +48,13 @@ def name_to_number(name):
         player_choice_number=3        
     if name == '剪刀' :
         player_choice_number=4
-    return player_choice_number                                                  
+    return player_choice_number                                                  
+
     
-    # 使用if语句将各游戏对象对应到不同的整数   
-
+    # 使用if语句将各游戏对象对应到不同的整数   
+
+
+
 def number_to_name(number):
    
     '''将整数 (0, 1, 2, 3, or 4)对应到游戏的不同对'''    
@@ -52,12 +70,16 @@ def number_to_name(number):
     if number==4:
         comp_choice='剪刀'
     return comp_choice
-                     
-    # 使用if语句将不同的整数对应到游戏的不同对象
-    
+                     
+
+    # 使用if语句将不同的整数对应到游戏的不同对象
+
+    
+
 def rpsls(player_choice_number,comp_number):
     
-    '''用户玩家任意给出一个选择，根据RPSLS游戏规则，判断对应的结果'''
+    '''用户玩家任意给出一个选择，根据RPSLS游戏规则，判断对应的结果
+'''
           
     if player_choice_number == 0 and (comp_number == 3 or comp_number == 4):
         result='您赢了！'
@@ -100,19 +122,32 @@ def result():
 result()
                              
     
-
-
-    # 输出"-------- "进行分割
+
+
+
+
+    # 输出"-------- "进行分割
+
     # 显示用户输入提示，用户通过键盘将自己的游戏选择对象输入，存入变量player_choice
     
-    # 调用name_to_number()函数将用户的游戏选择对象转换为相应的整数，存入变量player_choice_number
-
-    # 利用random.randrange()自动产生0-4之间的随机整数，作为计算机随机选择的游戏对象，存入变量comp_number
-
-    # 调用number_to_name()函数将计算机产生的随机数转换为对应的游戏对象
-
-    # 在屏幕上显示计算机选择的随机对象
-
-    # 利用if/elif/else 语句，根据RPSLS规则对用户选择和计算机选择进行判断，并在屏幕上显示判断结果
-
+    # 调用name_to_number()函数将用户的游戏选择对象转换为相应的整数，存入变量player_choice_number
+
+
+
+    # 利用random.randrange()自动产生0-4之间的随机整数，作为计算机随机选择的游戏对象，存入变量comp_number
+
+
+
+    # 调用number_to_name()函数将计算机产生的随机数转换为对应的游戏对象
+
+
+
+    # 在屏幕上显示计算机选择的随机对象
+
+
+
+    # 利用if/elif/else 语句，根据RPSLS规则对用户选择和计算机选择进行判断，并在屏幕上显示判断结果
+
+
+
     # 如果用户和计算机选择一样，则显示“您和计
